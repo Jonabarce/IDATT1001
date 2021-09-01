6c9646bba677d0c388fdff77a07324e340478417
Jonatan <89488655+Jonabarce@users.noreply.github.com>
2021-09-01 15:52:29 +0200

    Add files via upload

---
+++ gange1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+class ganga {
+public static void main (String [] args) {
+
+ Scanner myObj = new Scanner(System.in);
+    System.out.println (" Skriv inn onsket gangetabell fra 1 til 10 ");
+    int tall = myObj.nextInt(); 
+Scanner myObj2 = new Scanner(System.in);
+    System.out.println (" En tall hoyere enn dette tallet ");
+int tall2 = myObj2.nextInt();
+
+    // William sa du måtte endre språket
+for (int e = tall; e <= tall2; e++){
+for (int i = 1; i <= 10; i++){
+System.out.println (i + " x " + e + " = " + i * e);
+
+}
+}
+
+}
+
+}
