51208705204021557692ee2360e0527722b75e44
Jonatan Andre Vevang <89488655+Jonabarce@users.noreply.github.com>
2021-10-16 17:03:16 +0200

    Add files via upload

---
+++ Java/Oving/Oving 7/textTestKlient.java
@@ -0,0 +1,29 @@
+public class textTestKlient {
+    private static text2 test = new text2( " This is a test ");
+
+
+
+    public static void main (String[] args) {
+        System.out.println(test.totalletters() + " Totalletters test ok ");
+        System.out.println(test.averageWord() + " Averageword test ok ");
+        System.out.println(test.averageNumberWord() + " Averagenumberword test ok ");
+        System.out.println(test.replaceWord("a","e") + "Replaceword test ok ");
+        System.out.println(test.retrieveText() + " Retrieve test ok ");
+        System.out.println(test.upperCase() + " upperCase test ok ");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
